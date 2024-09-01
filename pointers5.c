26667c108c37c845c42b169527a51bf645124b01
Emir_Flying_Emu96 <emir.murat1337@gmail.com>
2024-09-02 01:13:14 +0300

    Create pointers5.c

---
+++ pointers5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 5;
+    int* p = &x;
+    *p = 6;
+    int** q = &p;
+    int*** r = &q;
+    printf("%d\n",*p);
+    printf("%d\n",*q);
+    printf("%d\n",**q);
+    printf("%d\n",**r);
+    printf("%d\n",***r);
+    ***r = 10;
+    printf("x = %d\n",x);
+    **q = *p +2;
+    printf("x = %d\n",x);
+    return 0;
+}
